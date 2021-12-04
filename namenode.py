887ce5bbb9d786397827ed83086022fe6f3cb7ac
Sai Jatin K <saijatin28@gmail.com>
2021-12-04 21:59:19 +0530

    updated namenode

--- namenode.py
+++ namenode.py
@@ -5,7 +5,7 @@ def ls(file, dir):
     curDir = dir.split('/')[1:]
     
     for dir in curDir:
-        if dir:
+        if dir in fs:
             fs = fs[dir]
         else:
             break
@@ -18,6 +18,7 @@ def mkdir(file, dir):
     fs = obj['fs']
     homeFS = fs
     curDir = dir.split('/')[1:]
+    print(curDir)
 
     for dir in curDir:
         if dir not in fs:
@@ -29,17 +30,28 @@ def mkdir(file, dir):
     with open(file, "w") as outfile:
         json.dump(obj, outfile)
 
+def rm():
+    pass
+
 def run(path):
     folderName = path[0]
-    configFile = path[1]
+    configFile = json.load(open(path[1]))
     logFile = folderName+'/NAMENODE/log.json'
+
+    blockSize = configFile['block_size']
+    numDatanodes = configFile['num_datanodes']
+    datanodeSize = configFile['datanode_size']
 
     # if the logFile was never populated, set it up
     try:
         json.load(open(logFile))
     except:
             with open(logFile, "w") as outfile:
-                json.dump({'fs':{}}, outfile)
+                datanodes = {}
+                for i in range(numDatanodes):
+                    datanodes[i+1] = datanodeSize
+
+                json.dump({'fs':{}, 'files':{}, 'datanodes':datanodes, 'lastEnteredDataNode':0}, outfile)
 
     while True:
         action = input()
@@ -52,10 +64,52 @@ def run(path):
             mkdir(logFile, action[1])
 
         elif action[0] == 'put':
-            pass
+            src = action[1]
+            dst = action[2]
+            
+            nameNodeData = json.load(open(logFile))
+            replication_factor = configFile['replication_factor']
+            curDataNode = nameNodeData['lastEnteredDataNode'] + 1
+            files = nameNodeData['files']
+            datanodesMeta = nameNodeData['datanodes'] 
+            dst = dst.split('/')[-1]
+            files[dst] = []
+
+            # check if file with same name exists already
+            if dst in files:
+                print('File with same name already exists.')
+                continue
+            
+            # breakup the src file into blocks, size(in kb) given in config file
+            with open(src) as f:
+                chunk = f.read(blockSize*1024)
+                while chunk:
+                    for i in range(replication_factor):
+                        if datanodesMeta[curDataNode] == 0:
+                            print('No space remaining. Deleting the file')
+                            rm(dst)
+
+                        datanode = folderName+'/DATANODE/DNODE'+str(curDataNode)
+                        openDataNode = open(datanode, 'a')
+                        
+                        start = openDataNode.tell()
+                        openDataNode.write(chunk)
+                        end = openDataNode.tell()-1
+                        
+                        files[dst].append({curDataNode: [start, end]})
+                        datanodesMeta[curDataNode] -= 1
+                        
+                        curDataNode += 1
+                        if curDataNode == numDatanodes+1:
+                            curDataNode = 1
+
+                    chunk = f.read(blockSize*1024)
+
         elif action[0] == 'cat':
             pass
+
         elif action[0] == 'rm':
             pass
+
         elif action[0] == 'rmdir':
             pass
